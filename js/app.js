39bbfe2e7f1f68a248dcc888f7e9cc7504c19c1b
noYes <noidontyesidon@example.com>
2023-02-03 16:42:08 +0800

    modularity test 2

--- js/app.js
+++ js/app.js
@@ -1,16 +1,13 @@
-import { uni_logo_url } from "../P_Site_Grid/js/util-var.js";
-import { event_json_url } from "../P_Site_Grid/js/util-var.js";
+import { uni_logo_url } from "../js/util-var.js";
+import { event_json_url } from "../js/util-var.js";
 
 import {
   HTMLUniLogos,
   CSSAffiliatedUniTransX,
   LoadAffiliatedUniversities,
-} from "../P_Site_Grid/js/load-affil-uni.js";
+} from "../js/load-affil-uni.js";
 
-import {
-  LoadMainEvents,
-  LoadEventDates,
-} from "../P_Site_Grid/js/load-main-events.js";
+import { LoadMainEvents, LoadEventDates } from "../js/load-main-events.js";
 
 window.addEventListener("DOMContentLoaded", (element) => {
   try {
